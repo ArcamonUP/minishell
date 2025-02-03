be151d895d0e8ea76fc7566ba554c2b331008c00
Kilfen Baridon <kbaridon@bess-f3r2s4.clusters.42paris.fr>
2025-02-03 15:44:32 +0100

    Improved structure of code

--- src/functions/ft_cd.c
+++ src/functions/ft_cd.c
@@ -1,35 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/02 15:43:37 by kbaridon          #+#    #+#             */
-/*   Updated: 2025/02/03 15:39:23 by kbaridon         ###   ########.fr       */
+/*   Created: 2025/02/03 15:31:16 by kbaridon          #+#    #+#             */
+/*   Updated: 2025/02/03 15:34:16 by kbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-# include <sys/types.h>
-
-typedef struct t_data
+void	ft_cd(char *line, t_data data)
 {
-	char	**envp;
-}	t_data;
-
-//functions
-void	ft_cd(char *line, t_data data);
-void	ft_echo(char *line);
-void	ft_env(t_data data);
-void	ft_export(char *line, t_data data);
-void	ft_pwd(void);
-void	ft_unset(char *line, t_data data);
-
-//utils
-void	ctrl_c(int sig);
-__pid_t	ft_exec(char *cmd, t_data data);
-
-#endif
+	(void)line;
+	(void)data;
+	return ;
+}
--- src/functions/ft_pwd.c
+++ src/functions/ft_pwd.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/02 15:43:37 by kbaridon          #+#    #+#             */
-/*   Updated: 2025/02/03 15:39:23 by kbaridon         ###   ########.fr       */
+/*   Created: 2025/02/03 15:32:32 by kbaridon          #+#    #+#             */
+/*   Updated: 2025/02/03 15:33:00 by kbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-# include <sys/types.h>
-
-typedef struct t_data
+void	ft_pwd(void)
 {
-	char	**envp;
-}	t_data;
-
-//functions
-void	ft_cd(char *line, t_data data);
-void	ft_echo(char *line);
-void	ft_env(t_data data);
-void	ft_export(char *line, t_data data);
-void	ft_pwd(void);
-void	ft_unset(char *line, t_data data);
-
-//utils
-void	ctrl_c(int sig);
-__pid_t	ft_exec(char *cmd, t_data data);
-
-#endif
+	return ;
+}
