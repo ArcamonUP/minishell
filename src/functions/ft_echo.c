e0cd9d3071edf2d12487fe87f0d7b3a510a6511e
Kilfen Baridon <kbaridon@paul-f4Ar5s7.clusters.42paris.fr>
2025-02-05 12:10:43 +0100

    Added ft_echo

--- src/functions/ft_echo.c
+++ src/functions/ft_echo.c
@@ -6,19 +6,37 @@
 /*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:31:41 by kbaridon          #+#    #+#             */
-/*   Updated: 2025/02/05 11:07:32 by kbaridon         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:46:21 by kbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
-//A faire
 void	ft_echo(char **line)
 {
-	//si + de 1 espace : on en met qu'un
-	//que des espaces = rien
-	//pas de retour à la ligne
-	(void)line;
+	int	i;
+
+	if (ft_strncmp(line[1], "-n", 2) == 0)
+	{
+		i = 2;
+		while (line[i])
+		{
+			ft_putstr_fd(line[i], 1);
+			if (line[i + 1])
+				ft_putstr_fd(" ", 1);
+			i++;
+		}
+		return ;
+	}
+	i = 1;
+	while (line[i])
+	{
+		ft_printf("%s", line[i]);
+		if (line[i + 1])
+			write(1, " ", 1);
+		i++;
+	}
+	write(1, "\n", 1);
 	return ;
 }
