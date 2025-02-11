188fc8d98c8f69d54ad27654db5ba89c9c9164d2
Adrien <chuadrien2003@gmail.com>
2025-02-11 18:24:30 +0100

    fix: parsing +splited redirect

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: achu <achu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:03:14 by achu              #+#    #+#             */
-/*   Updated: 2025/02/11 17:48:01 by achu             ###   ########.fr       */
+/*   Updated: 2025/02/11 18:22:55 by achu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,25 @@ t_node	*ft_node_new(char *str, e_type type);
 static t_node	*ft_parse_cmd(char ***token)
 {
 	t_node	*node;
-	t_node	*parent;
-	t_node	*right;
-	char	*op;
-	char	*file;
 
 	node = ft_node_new(**token, CMD);
 	if (!node)
 		return (NULL);
 	(*token)++;
+	return (node);
+}
+
+static t_node	*ft_parse_redir(char ***token)
+{
+	t_node	*node;
+	t_node	*parent;
+	t_node	*right;
+	char	*op;
+	char	*file;
+
+	node = ft_parse_cmd(token);
+	if (!node)
+		return (NULL);
 	while (**token && is_redir(**token))
 	{
 		op = **token;
@@ -57,14 +67,14 @@ static t_node	*ft_parse_pipe(char ***token)
 	t_node	*right;
 	char	*op;
 
-	node = ft_parse_cmd(token);
+	node = ft_parse_redir(token);
 	if (!node)
 		return (NULL);
 	while (**token && (ft_strncmp(**token, "|", 0) == 0))
 	{
 		op = **token;
 		(*token)++;
-		right = ft_parse_cmd(token);
+		right = ft_parse_redir(token);
 		if (!right)
 			return (NULL);
 		parent = (t_node *)malloc(sizeof(t_node));
