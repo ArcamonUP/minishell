01d3f9dc8fc552f03610fbba28ebe381383b5f28
Kilfen Baridon <kbaridon@paul-f4Ar6s2.clusters.42paris.fr>
2025-02-20 13:57:26 +0100

    First update pipex

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 17:23:56 by kbaridon          #+#    #+#             */
-/*   Updated: 2025/02/04 15:55:11 by kbaridon         ###   ########.fr       */
+/*   Updated: 2025/02/20 13:50:55 by kbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,23 @@ typedef struct t_pipex_data
 	int		fd[2];
 }	t_pipex_data;
 
+//init
+t_pipex_data	init_pipex(t_node node, char **envp);
+int				get_fd_file(t_node node, char c, int bonus);
+
 //pipex
-int		exec_bonus(t_pipex_data data);
-char	*get_path(char *cmd, char **envp);
+int				exec_bonus(t_pipex_data data);
+char			*get_path(char *cmd, char **envp);
 
 //utils
-void	error(char *msg, char *other);
-void	end(t_pipex_data data, int i);
-void	dispatch_pipex(char *line, t_pipex_data data, int fd[2]);
+void			error(char *msg, char *other);
+void			end(t_pipex_data data, int i);
+void			dispatch_pipex(char *line, t_pipex_data data, int fd[2]);
 
-void	wait_children(t_pipex_data data, pid_t p);
+void			wait_children(t_pipex_data data, pid_t p);
 
 //basics
-int		ft_tablen(char **tab);
-void	free_tab(char **tab);
+int				ft_tablen(char **tab);
+void			free_tab(char **tab);
 
 #endif
